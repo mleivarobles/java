5f27c8db56ea1b8ddea43a6aeb09443f333ed077
mleivarobles <mleiva.robles@gmail.com>
2023-09-12 11:41:01 -0300

    Uso de ArrayList experiencia 2.1

--- PGY2121_2023/ScooterArrayList/src/main/java/scooterarraylist/Empresa.java
+++ PGY2121_2023/ScooterArrayList/src/main/java/scooterarraylist/Empresa.java
@@ -15,6 +15,14 @@ public class Empresa {
     private String nombre;
     private ArrayList<Empleado> empleados;
     
+    //El Constructor me permite inicializar la lista de empleados cada vez
+    // que se cree una empresa nueva.
+    
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        this.empleados = new ArrayList<>();
+    }
+    
     public boolean agregarEmpleado(Empleado empleado){
         return false;
     }
