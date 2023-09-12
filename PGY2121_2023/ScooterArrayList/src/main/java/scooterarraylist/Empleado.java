e084b1f1aeb324f174676127559c24fbd9db3f71
mleivarobles <mleiva.robles@gmail.com>
2023-09-12 11:40:36 -0300

    Uso de ArrayList experiencia 2.1

---
+++ PGY2121_2023/ScooterArrayList/src/main/java/scooterarraylist/Empleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package scooterarraylist;
+
+/**
+ *
+ * @author mario
+ */
+public class Empleado {
+    
+    private int rut;
+    private String nombreCompleto;
+    private char genero;
+    private int annoServicio;
+    private int edad;
+    private Puesto puesto;
+ 
+}
---
+++ PGY2121_2023/ScooterArrayList/src/main/java/scooterarraylist/Empresa.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package scooterarraylist;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mario
+ */
+public class Empresa {
+    
+    private String nombre;
+    private ArrayList<Empleado> empleados;
+    
+    public boolean agregarEmpleado(Empleado empleado){
+        return false;
+    }
+    
+    public boolean buscarEmpleado(Empleado empleado){
+        return false;
+    }
+    
+    public boolean eliminarEmpleado(Empleado empleado){
+        return false;
+    }
+    
+    public void listarEmpleado(){
+    }
+}
