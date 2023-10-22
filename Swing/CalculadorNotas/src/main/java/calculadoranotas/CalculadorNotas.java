08534473480cf3ee8be613897cfb6a4beffa0348
mleivarobles <mleiva.robles@gmail.com>
2023-10-22 13:58:05 -0300

    Se agrega ejemplo de uso Swing

---
+++ Swing/CalculadorNotas/src/main/java/calculadoranotas/CalculadorNotas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package calculadoranotas;
+
+/**
+ *
+ * @author mario
+ */
+public class CalculadorNotas {
+
+    public static void main(String[] args) {
+        Principal calculadora = new Principal();
+        calculadora.setVisible(true);
+        calculadora.setLocationRelativeTo(null);
+    }
+}
---
+++ Swing/PrimeraInterfaz/src/main/java/primerainterfaz/PrimeraInterfaz.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primerainterfaz;
+
+/**
+ *
+ * @author mario
+ */
+public class PrimeraInterfaz {
+
+    public static void main(String[] args) {
+        
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null); //
+    }
+}
