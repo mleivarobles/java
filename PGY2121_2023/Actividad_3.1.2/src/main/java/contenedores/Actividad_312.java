df258468390512106a980dfcb53724c735cd2d10
mleivarobles <mleiva.robles@gmail.com>
2023-10-24 13:32:20 -0300

    Se actualizan ejercicios  en POO

---
+++ PGY2121_2023/Actividad_3.1.2/src/main/java/contenedores/Actividad_312.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package contenedores;
+
+/**
+ *
+ * @author mario
+ */
+public class Actividad_312 {
+
+    public static void main(String[] args) {
+        Ventana1 ventana1 = new Ventana1();
+        ventana1.setVisible(true);
+        ventana1.setLocationRelativeTo(null);
+        
+        Ventana4 ventana4 = new Ventana4();
+        ventana4.setVisible(true);
+        ventana4.setLocationRelativeTo(null);
+        
+    }
+}
