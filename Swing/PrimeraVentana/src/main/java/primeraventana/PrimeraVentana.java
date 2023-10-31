4dab01a30541d21c4330702b18a55ff2ef827927
mleivarobles <mleiva.robles@gmail.com>
2023-10-31 13:02:15 -0300

    Se agregan proyectos en carpeta Swing

---
+++ Swing/PrimeraVentana/src/main/java/primeraventana/PrimeraVentana.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primeraventana;
+
+/**
+ *
+ * @author mario
+ */
+public class PrimeraVentana {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
