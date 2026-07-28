353c84f4021e6483272426e5ff6403e9ddecd9a0
MARIO ALEJANDRO LEIVA ROBLES <mleiva@prochile.gob.cl>
2026-07-27 22:50:18 -0400

    Se agrega carpeta para POO-2026

---
+++ PRE-24067-1_POO/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola, POO!");
+        System.out.println("Bienvenidos al curso de Programación Orientada a Objetos");
+        System.out.println("Ingeniería en Informática - UMC 2026");
+        System.out.println("-------------------------------------------");
+        System.out.println("Lenguaje: Java 17");
+        System.out.println("IDE: Visual Studio Code");
+        System.out.println("Semestre: IV - Segundo semestre 2026");
+    }
+}
