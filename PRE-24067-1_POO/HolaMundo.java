c2cb9e2d60ba32bc93bc35cdf452e95f591d6241
Mario Leiva Robles <mleiva.robles@gmail.com>
2026-07-28 11:11:52 -0400

    Se actualiza versión de JDK

--- PRE-24067-1_POO/HolaMundo.java
+++ PRE-24067-1_POO/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
         System.out.println("Bienvenidos al curso de Programación Orientada a Objetos");
         System.out.println("Ingeniería en Informática - UMC 2026");
         System.out.println("-------------------------------------------");
-        System.out.println("Lenguaje: Java 17");
+        System.out.println("Lenguaje: Temurin 25");
         System.out.println("IDE: Visual Studio Code");
         System.out.println("Semestre: IV - Segundo semestre 2026");
     }
